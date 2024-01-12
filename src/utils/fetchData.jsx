d4eab264f63ccaf7dd1d8a492215314afc148d64
HKES-04 <husseinkhawalelsultan@gmail.com>
2024-01-11 21:31:21 -0500

    update

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
-    "X-RapidAPI-Key": "3ac97e07edmsh59ebfd890d77fe8p138f03jsn19d2d94c2dbb",
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "34574ee236msh31516d817f31698p1bb095jsn38bfd27d610c",
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_YOUTUBE_API_KEY,
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
